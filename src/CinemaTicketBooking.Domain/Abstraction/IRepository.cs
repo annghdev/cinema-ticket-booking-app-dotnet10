d9a6d02591f518a7f20ca925259a4afc0c1ac25b
John Doe <annghdev@gmail.com>
2026-04-11 13:39:27 +0700

    Add generic repository interface

---
+++ src/CinemaTicketBooking.Domain/Abstraction/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace CinemaTicketBooking.Domain;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default);
+    IQueryable<TEntity> GetQueryFilter();
+}
