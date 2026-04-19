1621f459832ea104bea740e41e778040f33c7d15
John Doe <annghdev@gmail.com>
2026-04-19 13:37:24 +0700

    chore: move sample requests to ApiEndpoints folders

---
+++ src/CinemaTicketBooking.WebServer/ApiEndpoints/PaymentEndpoints.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemaTicketBooking.WebServer.ApiEndpoints;
+
+public static class PaymentEndpoints
+{
+    public static void MapPaymentEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/api/payments")
+            .WithTags("Payments");
+
+        group.MapGet("/momo-callback", MomoCallback)
+            .AllowAnonymous();
+    }
+
+    public static async Task<IResult> MomoCallback()
+    {
+        return Results.Ok();
+    }
+}
